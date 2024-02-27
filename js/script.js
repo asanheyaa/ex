41a83caf5920373965f74c2ea5173e3e096e529f
asanheyaa <122150617+asanheyaa@users.noreply.github.com>
2024-02-27 20:39:57 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -108,3 +108,14 @@ function activeMenu() {
 }
 
 activeMenu()
+
+const logo = document.querySelector('.header__logo');
+
+
+logo.addEventListener('click', (e)=>{
+	e.preventDefault()
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+})
